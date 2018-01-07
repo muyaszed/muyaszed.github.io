f75325aa6801a8575e345deb4f15eb9fcb1e3d16
Yazed Jamal <muyaszed@gmail.com>
2018-01-07 21:29:25 +0000

    added dig dug clone

---
+++ myDigDug/js/resources.js
@@ -0,0 +1,42 @@
+game.resources = [
+    {
+        "name": "bg",
+        "type": "audio",
+        "src": "data/bgm/"
+    },
+    {
+        "name": "dead",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "fire",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "monster",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "digger",
+        "type": "image",
+        "src": "data/img/digger.png"
+    },
+    {
+        "name": "monster",
+        "type": "image",
+        "src": "data/img/monster.png"
+    },
+    {
+        "name": "PressStart2P",
+        "type": "image",
+        "src": "data/fnt/PressStart2P.png"
+    },
+    {
+        "name": "PressStart2P",
+        "type": "binary",
+        "src": "data/fnt/PressStart2P.fnt"
+    }
+];
