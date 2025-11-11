3fec8baf4f2f964d83d002abca5788a694a0ad5a
Yazed Jamal <yazedjamal@Yazeds-MacBook-Air.local>
2025-11-11 09:18:51 +0000

    refactor title

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-    title: "Yazed Jamal's Site",
+    title: "Yazed Jamal's Portfolio",
     description:
         "Writes code for a better user experience. Lifelong learner and Engineer of many things",
 };
